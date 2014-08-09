d54a8e223ef78649f341507aefd15a5c1712277b
Peter de Croos <cultofmetatron@aumlogic.com>
2014-08-08 20:33:40 -0700

    removed console.logs

--- test/cascade-spec.js
+++ test/cascade-spec.js
@@ -7,57 +7,66 @@ describe('cascade', function() {
 
   it('merges 2 functions together like a compose', function(done) {
     var fn1 = function(req, res, next) {
-      console.log(req, res, next)
+      req.should.equal(2);
+      res.should.equal(3);
       next();
     };
 
     var fn2 = function(req, res, next){
-      console.log('fn2');
+      req.should.equal(2);
+      res.should.equal(3);
       next();
     };
 
     var fn3 = function(req, res, next) {
-      console.log('fn3');
       req.should.equal(2);
       res.should.equal(3);
-      console.log('yay', req, res);
       next();
     };
   
-    cascade(fn1, fn2, fn3)(2, 3, function() { console.log('usp'); done(); });
+    cascade(fn1, fn2, fn3)(2, 3, function(req, res, next) { 
+      req.should.equal(2);
+      res.should.equal(3);
+      done();
+    });
   });
 
   it('lets me nest cascades', function(done) {
-    console.log('nesting#####')
     var fn1 = function(req, res, next) {
-      console.log(req, res, next)
+      req.should.equal(2);
+      res.should.equal(3);
       next();
     };
 
     var fn2 = function(req, res, next){
-      console.log('fn2');
+      req.should.equal(2);
+      res.should.equal(3);
       next();
     };
 
     var fn2_1 = function(req, res, next){
-      console.log('fn2_1');
+      req.should.equal(2);
+      res.should.equal(3);
       next();
     };
 
     var fn2_2 = function(req, res, next){
-      console.log('fn2_2');
+      req.should.equal(2);
+      res.should.equal(3);
       next();
     };
 
     var fn3 = function(req, res, next) {
-      console.log('fn3');
       req.should.equal(2);
       res.should.equal(3);
-      console.log('yay', req, res);
       next()
     };
 
-    cascade(fn1, fn2, cascade(fn2_1, fn2_2) , fn3)(2, 3, function() { console.log('usp'); done(); });
+    cascade(fn1, fn2, cascade(fn2_1, fn2_2) , fn3)(2, 3, function(req, res) { 
+      req.should.equal(2);
+      res.should.equal(3);
+      done(); 
+    });
   });
 
 
